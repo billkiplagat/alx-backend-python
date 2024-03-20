156facd77b9cc908d8ab2f90157a564d4d431eca
billkiplagat <bill.kiplagat.7@gmail.com>
2024-03-20 05:23:13 -0400

    Completed tasks

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Task 2's module.
+"""
+import asyncio
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Executes async_comprehension 4 times and measures the
+    total execution time.
+    """
+    start_time = time.time()
+
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+
+    end_time = time.time()
+
+    total_time = end_time - start_time
+    return total_time
